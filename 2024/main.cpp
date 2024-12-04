af22dc6d0624b036c856e0a782a583772a6e3de1
BrainStone <yannick@tedworld.de>
2024-12-04 13:30:07 +0100

    Allow one input file per day too, but always try to use the per problem file first

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -6,7 +6,9 @@
 #include "util.hpp"
 
 // Mapping of all solution
-const std::map<std::string, std::function<std::string(std::istream&)>> solutions{{"1_1", day1_1}, {"1_2", day1_1}};
+const std::unordered_map<std::string, std::function<std::string(std::istream&)>> solutions{
+    {"1_1", day1_1}, {"1_2", day1_2},  // Day 1 solutions
+};
 
 int main(int argc, char** argv) {
 	// Change working dir to be relative to ourselves
@@ -17,7 +19,7 @@ int main(int argc, char** argv) {
 
 	std::string problem;
 
-	if (argc < 1) {
+	if (argc <= 1) {
 		std::cout << "Which problem to run (format <day>_<problem>, like 12_2): ";
 		std::cin >> problem;
 		std::cout << std::endl;
--- 2024/util.cpp
+++ 2024/util.cpp
@@ -5,13 +5,23 @@
 #include <iostream>
 
 wrapped_istream open_input(const std::string& problem) {
-	std::filesystem::path target_file{"inputs/day" + problem + ".txt"};
+	std::string day = problem.substr(0, problem.find('_'));
 
-	if (is_regular_file(target_file)) {
-		return {new std::ifstream{target_file}, [](std::istream* ptr) { delete ptr; }};
+	std::filesystem::path specific_problem_file{"inputs/day" + problem + ".txt"};
+	std::filesystem::path day_problem_file{"inputs/day" + day + ".txt"};
+
+	std::filesystem::path problem_file;
+
+	if (is_regular_file(specific_problem_file)) {
+		problem_file = specific_problem_file;
+	} else if (is_regular_file(day_problem_file)) {
+		problem_file = day_problem_file;
 	} else {
-		std::cerr << "Could not find input file " << target_file << "! Falling back to manual input:" << std::endl;
+		std::cerr << "Could not find input files " << specific_problem_file << " or " << day_problem_file
+		          << "! Falling back to manual input:" << std::endl;
 
 		return {&std::cin, [](std::istream*) {}};
 	}
+
+	return {new std::ifstream{problem_file}, [](std::istream* ptr) { delete ptr; }};
 }
