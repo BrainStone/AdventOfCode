999f0e15b103716e08d3a24e5bff975ba40dcd25
BrainStone <yannick@tedworld.de>
2024-12-04 13:11:32 +0100

    Allow passing the day as CLI param

--- 2024/main.cpp
+++ 2024/main.cpp
@@ -9,7 +9,7 @@
 const std::map<std::string, std::function<std::string(std::istream &)>>
     solutions{{"1_1", day1_1}, {"1_2", day1_1}};
 
-int main() {
+int main(int argc, char **argv) {
   // Change working dir to be relative to ourselves
   std::filesystem::path exec{__FILE__};
   std::filesystem::path exec_dir{exec.parent_path()};
@@ -17,13 +17,20 @@ int main() {
   std::filesystem::current_path(exec_dir);
 
   std::string problem;
-  std::cout << "Which problem to run (format <day>_<problem>, like 12_2): ";
-  std::cin >> problem;
-  std::cout << std::endl;
+
+  if (argc < 1) {
+    std::cout << "Which problem to run (format <day>_<problem>, like 12_2): ";
+    std::cin >> problem;
+    std::cout << std::endl;
+  } else {
+    problem = argv[1];
+    std::cout << "Selected problem day" << problem << " from CLI params." << std::endl;
+  }
 
   std::string solution{solutions.at(problem)(*open_input(problem))};
 
-  std::cout << "\n\nSolution for day" << problem << ": " << solution;
+  std::cout << "\n\nSolution for day" << problem << ": " << solution
+            << std::endl;
 
   return 0;
 }
