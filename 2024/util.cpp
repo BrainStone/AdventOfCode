c3357ecb4e695fc11673cfe5e9118ac5afac44af
BrainStone <yannick@tedworld.de>
2024-12-08 06:30:22 +0100

    Extract letter_grid class to utils in preparation for day6

--- 2024/util.cpp
+++ 2024/util.cpp
@@ -25,3 +25,115 @@ wrapped_istream open_input(const std::string& problem) {
 
 	return {new std::ifstream{problem_file}, [](std::istream* ptr) { delete ptr; }};
 }
+
+point operator+(const point& lhs, const point& rhs) {
+	return {lhs[0] + rhs[0], lhs[1] + rhs[1]};
+}
+
+point& operator+=(point& lhs, const point& rhs) {
+	lhs[0] += rhs[0];
+	lhs[1] += rhs[1];
+
+	return lhs;
+}
+
+point operator-(const point& lhs, const point& rhs) {
+	return {lhs[0] - rhs[0], lhs[1] - rhs[1]};
+}
+
+point& operator-=(point& lhs, const point& rhs) {
+	lhs[0] -= rhs[0];
+	lhs[1] -= rhs[1];
+
+	return lhs;
+}
+
+point operator*(const point& lhs, long long rhs) {
+	return {lhs[0] * rhs, lhs[1] * rhs};
+}
+
+point& operator*=(point& lhs, long long rhs) {
+	lhs[0] *= rhs;
+	lhs[1] *= rhs;
+
+	return lhs;
+}
+
+void letter_grid::check_line(const std::string& line) const {
+	if (grid.empty()) return;
+
+	const size_t width = this->width();
+	const size_t length = line.size();
+
+	if (length != width)
+		throw std::length_error("Expected length of line to be " + std::to_string(width) + " but it was " +
+		                        std::to_string(length));
+}
+
+letter_grid::letter_grid() : grid() {}
+
+void letter_grid::add_line(const std::string& line) {
+	check_line(line);
+
+	grid.push_back(line);
+}
+
+void letter_grid::add_line(std::string&& line) {
+	check_line(line);
+
+	grid.push_back(line);
+}
+
+[[nodiscard]] size_t letter_grid::width() const {
+	if (grid.empty()) return 0;
+
+	return grid[0].size();
+}
+
+[[nodiscard]] size_t letter_grid::height() const {
+	return grid.size();
+}
+
+[[nodiscard]] std::string& letter_grid::operator[](size_t row) {
+	return grid[row];
+}
+
+[[nodiscard]] const std::string& letter_grid::operator[](size_t row) const {
+	return grid[row];
+}
+
+[[nodiscard]] char& letter_grid::operator[](const point& position) {
+	return grid[position[0]][position[1]];
+}
+
+[[nodiscard]] const char& letter_grid::operator[](const point& position) const {
+	return grid[position[0]][position[1]];
+}
+
+[[nodiscard]] bool letter_grid::is_valid_point(const point& position) const {
+	return (position[0] >= 0 && position[0] < this->height()) && (position[1] >= 0 && position[1] < this->width());
+}
+
+std::istream& operator>>(std::istream& is, letter_grid& grid) {
+	std::string line;
+
+	while (is.good()) {
+		std::getline(is, line);
+
+		if (line.empty()) break;
+
+		grid.add_line(std::move(line));
+	}
+
+	return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const letter_grid& grid) {
+	const size_t rows = grid.height();
+
+	for (size_t row = 0; row < rows; ++row) {
+		os << grid[row] << '\n';
+	}
+
+	return os;
+}
--- 2024/util.hpp
+++ 2024/util.hpp
@@ -1,10 +1,48 @@
 #pragma once
 
+#include <array>
 #include <functional>
 #include <istream>
 #include <memory>
 #include <string>
 
 using wrapped_istream = std::unique_ptr<std::istream, std::function<void(std::istream*)>>;
+using point = std::array<long long, 2>;
 
 wrapped_istream open_input(const std::string& problem);
+
+point operator+(const point& lhs, const point& rhs);
+point& operator+=(point& lhs, const point& rhs);
+
+point operator-(const point& lhs, const point& rhs);
+point& operator-=(point& lhs, const point& rhs);
+
+point operator*(const point& lhs, long long rhs);
+point& operator*=(point& lhs, long long rhs);
+
+class letter_grid {
+private:
+	std::vector<std::string> grid;
+
+	void check_line(const std::string& line) const;
+
+public:
+	letter_grid();
+
+	void add_line(const std::string& line);
+	void add_line(std::string&& line);
+
+	[[nodiscard]] size_t width() const;
+	[[nodiscard]] size_t height() const;
+
+	[[nodiscard]] std::string& operator[](size_t row);
+	[[nodiscard]] const std::string& operator[](size_t row) const;
+
+	[[nodiscard]] char& operator[](const point& position);
+	[[nodiscard]] const char& operator[](const point& position) const;
+
+	[[nodiscard]] bool is_valid_point(const point& position) const;
+};
+
+std::istream& operator>>(std::istream& is, letter_grid& grid);
+std::ostream& operator<<(std::ostream& os, const letter_grid& grid);
