817315689099a191fa080fd10486cecd2c98a1dd
BrainStone <yannick@tedworld.de>
2024-12-09 01:58:02 +0100

    Allow any int type to be passed to the registry

--- 2024/registry.cpp
+++ 2024/registry.cpp
@@ -5,10 +5,6 @@ solution_registry& solution_registry::instance() {
 	return registry;
 }
 
-void solution_registry::register_solution(const std::string& name, const int_function& func) {
-	solutions[name] = [func](std::istream& input) { return std::to_string(func(input)); };
-}
-
 void solution_registry::register_solution(const std::string& name, const string_function& func) {
 	solutions[name] = func;
 }
--- 2024/registry.hpp
+++ 2024/registry.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <functional>
 #include <istream>
 #include <string>
@@ -8,12 +9,20 @@
 class solution_registry {
 public:
 	using string_function = std::function<std::string(std::istream&)>;
-	using int_function = std::function<int(std::istream&)>;
-	
+
 	static solution_registry& instance();
 
-	void register_solution(const std::string& name, const int_function& func);
 	void register_solution(const std::string& name, const string_function& func);
+
+	template <std::integral T>
+	void register_solution(const std::string& name, const std::function<T(std::istream&)>& func) {
+		register_solution(name, [func](std::istream& input) { return std::to_string(func(input)); });
+	}
+
+	template <std::integral T>
+	void register_solution(const std::string& name, T (*func)(std::istream&)) {
+		register_solution(name, std::function<T(std::istream&)>{func});
+	}
 
 	const string_function& get_solution(const std::string& problem) const;
 
@@ -24,10 +33,10 @@ private:
 	solution_registry() = default;
 };
 
-#define REGISTER_SOLUTION(name, func)                                \
-	static bool _##name##_registered = [] {                          \
+#define REGISTER_SOLUTION(name, func)                                 \
+	static bool _##name##_registered = [] {                           \
 		solution_registry::instance().register_solution(#name, func); \
-		return true;                                                 \
+		return true;                                                  \
 	}();
 
 #define REGISTER_DAY(problem_day)                            \
